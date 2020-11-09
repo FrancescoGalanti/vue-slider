447919e754e4370550baa553c2e298007a89d139
FrancescoGalanti <galanti_francesco>
2020-11-09 16:19:36 +0100

    function previuos and next fatta

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -12,23 +12,35 @@
      // Index value used for swtiching images //
        indexImg: 0,
        // array of images //
-       images: {
+       images: [
             "./images/image1.jpg",
             "./images/image2.jpg",
             "./images/image3.jpg",
-            "./images/image4.jpg",
+            "./images/image4.jpg"
+         ],
        },
-       // Methods //
+              // Methods //
        methods: {
          // callback function that manage  the image change with the next one //
          nextImages(){
             // adding 1 for swtiching image //
             this.indexImg += 1;
-            // 
-            if(this.)
-         }
+            // condition that manages the exchange with the image of the maximum index value with the minimum value //
+            if(this.indexImg > (this.images.length - 1)){
+              this.indexImg = 0;
+            }
+         },
+         // callback function that manage  the image change with the previous one //
+         prevImages(){
+          // decrease by one //
+          this.indexImg -= 1;
+          // condition that manages the exchange with the image of the minimum index value with the maximum value //
+          if(this.indexImg < 0){
+            this.indexImg = this.images.length - 1;
+          }
+        },
 
        }
 
-   }
+
  })
