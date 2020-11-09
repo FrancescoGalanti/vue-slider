e3a7827834f81a44b05320dcd3e530dbb1812147
FrancescoGalanti <galanti_francesco>
2020-11-09 15:58:15 +0100

    condizioni

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,10 +6,29 @@
  // vue //
 
 // dom in which we operate in vue //
- // var app = new Vue({
- //   el: '#app',
- //   data: {
- //
- //
- //   }
- // })
+ var app = new Vue({
+   el: '#app',
+   data: {
+     // Index value used for swtiching images //
+       indexImg: 0,
+       // array of images //
+       images: {
+            "./images/image1.jpg",
+            "./images/image2.jpg",
+            "./images/image3.jpg",
+            "./images/image4.jpg",
+       },
+       // Methods //
+       methods: {
+         // callback function that manage  the image change with the next one //
+         nextImages(){
+            // adding 1 for swtiching image //
+            this.indexImg += 1;
+            // 
+            if(this.)
+         }
+
+       }
+
+   }
+ })
